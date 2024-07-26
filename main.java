3dde9eb817e6c5e01b752131fd8fb95a93d8b3f7
Dharmik Gohil <86297731+DharmikGohil@users.noreply.github.com>
2024-07-26 11:42:49 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,6 @@
 class demo{
   public static void main(String[] args){
-<<<<<<< HEAD
-    int c = 25;
-=======
-    int age = 18;
->>>>>>> bc0f97272af095de6115a73fc3d39925a63c73c4
+    int hey = 100;
     System.out.print(age);
   }
 }
